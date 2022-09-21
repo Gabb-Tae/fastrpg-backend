0b16a46e4612922ff4beaf3975f0a25bea7d2f27
Gabriel <gabbmaia2004@gmail.com>
2022-09-20 22:39:20 -0300

    comit raças, itens

--- core/models.py
+++ core/models.py
@@ -55,12 +55,12 @@ class rpgrace(models.Model):
     size = models.CharField(max_length=20)
     speed = models.CharField(max_length=20)
     languages = models.CharField(max_length=60)
-    strength = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(30)])
-    dexterity = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(30)])
-    constitution = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(30)])
-    intelligence = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(30)])
-    wisdom = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(30)])
-    charism = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(30)])
+    strength = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(30)])
+    dexterity = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(30)])
+    constitution = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(30)])
+    intelligence = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(30)])
+    wisdom = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(30)])
+    charism = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(30)])
 
     def __str__(self):
         return self.name
